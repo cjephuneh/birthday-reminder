371f1308952892c3857218d0d0242ff654d027cd
caleb jephuneh <calebjephunneh3@gmail.com>
2022-12-01 00:04:57 +0300

    cooode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { useState } from 'react'
 import List from './List'
 import data from './data'
 
 function App() {
+  const [people, setPeople] = useState(data)
   return (
     <main>
       <section className='container'>
-        <h3>0 birthdays today</h3>
+        <h3>{people.length} birthdays today</h3>
       <List />
       <button onClick={() => console.log('You clicked me')}>clear all</button>
       </section>
