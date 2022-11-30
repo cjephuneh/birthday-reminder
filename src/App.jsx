14e4ba4354f43b13b68220d3d892a7b24666c576
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-30 23:57:18 +0300

    coode

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <section className='container'>
         <h3>0 birthdays today</h3>
       <List />
+      <button onClick={() => console.log('You clicked me')}>clear all</button>
       </section>
     </main>
   )
