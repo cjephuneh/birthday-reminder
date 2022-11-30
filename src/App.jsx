f24f259ad6fbd4ff9f652fb9c39fd1cd8a5eb151
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-30 16:05:00 +0300

    favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 import List from './List'
+import data from './data'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <List />
