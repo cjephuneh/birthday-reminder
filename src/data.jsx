e35565f906bb8bbe568b85523f5a62a03e523786
caleb jephuneh <calebjephunneh3@gmail.com>
2022-12-01 01:04:14 +0300

    code

--- src/data.jsx
+++ src/data.jsx
@@ -1,36 +1,36 @@
 export default [
     {
       id: 1,
-      name: 'Bertie Yates',
-      age: 29,
+      name: 'Chelangat Ngetich',
+      age: 20,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
     },
     {
       id: 2,
-      name: 'Hester Hogan',
-      age: 32,
+      name: 'Jones Mbogholi',
+      age: 23,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
     },
     {
       id: 3,
-      name: 'Larry Little',
-      age: 36,
+      name: 'Caleb Laitetei',
+      age: 20,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
     },
     {
       id: 4,
-      name: 'Sean Walsh',
-      age: 34,
+      name: 'Marvin Onyango',
+      age: 24,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
     },
     {
       id: 5,
-      name: 'Lola Gardner',
-      age: 29,
+      name: 'Sinai Mattah',
+      age: 17,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
     },
